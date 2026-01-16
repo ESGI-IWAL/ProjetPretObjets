084bd7799362eeb9fa094b33c265621a203887f0
remcor <r.cornet@myskolae.fr>
2026-01-16 23:11:53 +0100

    doc: procédure d'installation

--- src/main/java/com/aliw/pretemoica/config/SecurityConfig.java
+++ src/main/java/com/aliw/pretemoica/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-package com.aliw.pretemoica.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.context.annotation.Bean;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/h2-console/**"))
-                .headers(headers -> headers
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-        return http.build();
-    }
-}
+//package com.aliw.pretemoica.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.context.annotation.Bean;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/h2-console/**", "/users/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .csrf(csrf -> csrf
+//                        .ignoringRequestMatchers("/h2-console/**"))
+//                .headers(headers -> headers
+//                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+//        return http.build();
+//    }
+//}
