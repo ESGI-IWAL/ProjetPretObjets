33f8ab83a3f8f99b81432038e02b276bef99b8a1
Lolippie <charlot.noemie72@gmail.com>
2026-05-15 15:23:43 +0200

    feat: modif et suite appels routes

--- frontend/app/pages/lending/new.vue
+++ frontend/app/pages/lending/new.vue
@@ -1,11 +1,4 @@
 <script setup lang="ts">
-import type { ICreateLendingDto } from '~/dto/lending/create.dto';
-import { createLending } from '~/services/lending';
-
-
-const handleCreationSubmit = (dto : ICreateLendingDto) => {
-    createLending(dto)
-}
 
 </script>
 <template>
