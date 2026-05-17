31b192a964189e0eaea30b5522f57e66b03178b8
Lolippie <charlot.noemie72@gmail.com>
2026-05-17 14:18:36 +0200

    feat: debut lending

---
+++ frontend/app/components/Lending/SearchForm.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { ISearchLendingDto } from '~/dto/lending/search.dto';
+import { ELendingStatus } from '~/enums/lending/status.enum';
+
+const emit = defineEmits(['search'])
+
+const form = reactive<ISearchLendingDto>({
+    objectName: "",
+    borrowerName: "",
+    startDate: new Date(),
+    endDate: new Date(),
+    status: ELendingStatus.ACTIVE
+})
+
+watch(form, () => {
+    emit('search', form)
+}, { deep: true })
+</script>
+
+<template>
+    <form @submit.prevent>
+        <input v-model="form.objectName" placeholder="Nom de l'objet"/>
+        <input v-model="form.borrowerName" placeholder="Nom de l'emprunteur"/>
+        <input type="date" v-model="form.startDate"/>
+        <input type="date" v-model="form.endDate"/>
+        <select v-model="form.status">
+            <option value="">Tous</option>
+            <option value="ACTIVE">Actifs</option>
+            <option value="COMPLETED">Terminés</option>
+            <option value="CANCELED">Annulés</option>
+        </select>
+        <button type="submit">Rechercher</button>
+    </form>
+</template>
+
+
+<style>
+
+</style>
