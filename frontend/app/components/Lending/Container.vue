debefadc660afe20c0eb7cb225870fd20136c7dd
Lolippie <charlot.noemie72@gmail.com>
2026-05-17 14:18:36 +0200

    feat: debut lending

--- frontend/app/components/Lending/Container.vue
+++ frontend/app/components/Lending/Container.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import type { ISearchLendingDto } from '~/dto/lending/search.dto';
+import type { ILending } from '~/types/lending'
 
+defineProps<{
+  lendings: ILending[],
+}>()
+
+const emit = defineEmits(['search'])
+
+function search(dto : ISearchLendingDto) {
+    emit('search', dto)
+}
 </script>
+
 <template>
-  
+    <LendingSearchForm @search="search" />
+    <div v-for="lending in lendings" :key="lending.id">
+        <LendingCard :lending="lending"/>
+    </div>
 </template>
 
 
