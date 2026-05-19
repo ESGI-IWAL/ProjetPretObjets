ed08ce3f558c2bfa1f76730e7e51499d590a9625
Lolippie <charlot.noemie72@gmail.com>
2026-05-19 23:08:18 +0200

    fix: api + enlever middleware (tempo)

--- frontend/app/services/object.ts
+++ frontend/app/services/object.ts
@@ -1,46 +1,47 @@
 import type { ICreateObjectDto } from "~/dto/object/create.dto";
 import type { ISearchObjectDto } from "~/dto/object/search.dto";
 import type { IUpdateObjectDto } from "~/dto/object/update.dto";
+import type { IObject } from "~/types/object";
 
-  const api = useApi()
+const api = () => useNuxtApp().$api
 
 export const createObject = async (dto: ICreateObjectDto) => {
-  return await api("/objects", {
+  return await api()("/objects", {
     method: "POST",
     body: dto
   })
 }
 
 export const getObjects = async () => {
-  return await api("/objects")
+  return await api()<IObject[]>("/objects")
 }
 
 export const getObjectById = async (id: string) => {
-  return await api(`/objects/${id}`)
+  return await api()<IObject>(`/objects/${id}`)
 }
 
 export const searchObjects = async (searchParams: ISearchObjectDto) => {
-  return await api("/objects/search", {
+  return await api()<IObject[]>("/objects/search", {
     method: "POST",
     body: searchParams
   })
 }
 
 export const updateObject = async (id: string, dto: IUpdateObjectDto) => {
-  return await api(`/objects/${id}`, {
+  return await api()<IObject>(`/objects/${id}`, {
     method: "PUT",
     body: dto
   })
 }
 
 export const deleteObject = async (id: string) => {
-  return await api(`/objects/${id}`, {
+  return await api()(`/objects/${id}`, {
     method: "DELETE"
   })
 }
 
-export const historyObject = async (id: string) => {
-  return await api(`/objects/${id}/history`)
+export const historyObjectById = async (id: string) => {
+  return await api()<IObject[]>(`/objects/${id}/history`)
 }
 
 // GET all
--- frontend/app/services/user.ts
+++ frontend/app/services/user.ts
@@ -2,28 +2,32 @@ import type { ICreateUserDto } from "~/dto/user/create.dto";
 import type { ISearchUserDto } from "~/dto/user/search.dto";
 import type { IUser } from "~/types/user";
 
-  const api = useApi()
+const api = () => useApi()
 
 export const createUser = async (dto: ICreateUserDto) => {
-    return await api("/users", {
+    return await api()("/users", {
         method: "POST",
         body: dto
     });
 }
 
+export const getUsers = async () => {
+    return await api()<IUser[]>("/users")
+}
+
 export const searchUser = async (dto : ISearchUserDto) => {
-    return await api<IUser[]>("/users/search", {
+    return await api()<IUser[]>("/users/search", {
         method: "POST",
         body: dto
     });
 }
 
 export const getUserById = async (id: string) => {
-    return await api<IUser>(`/users/${id}`)
+    return await api()<IUser>(`/users/${id}`)
 }
 
 export const getCurrentUser = async () => {
-    return await api<IUser>('/me')
+    return await api()<IUser>('/me')
 }
 // GET by ID
 // POST RECHERCHE
