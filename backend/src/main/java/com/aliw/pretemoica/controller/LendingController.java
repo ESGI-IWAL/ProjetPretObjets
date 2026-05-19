5745ce399cc544acb1820cf577cd8b219a685ba5
Lolippie <charlot.noemie72@gmail.com>
2026-05-19 23:06:25 +0200

    fix: nommage back

--- backend/src/main/java/com/aliw/pretemoica/controller/LendingController.java
+++ backend/src/main/java/com/aliw/pretemoica/controller/LendingController.java
@@ -1,38 +1,46 @@
 package com.aliw.pretemoica.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.aliw.pretemoica.dto.LendingDto;
 import com.aliw.pretemoica.mapper.LendingMapper;
 import com.aliw.pretemoica.service.LendingService;
-import java.util.List;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/lendings")
+@RequestMapping("/lending")
 public class LendingController {
 
-  private final LendingService lendingService;
+  private final LendingService lendingervice;
 
-  public LendingController(LendingService lendingService) {
-    this.lendingService = lendingService;
+  public LendingController(LendingService lendingervice) {
+    this.lendingervice = lendingervice;
   }
 
   @GetMapping
-  public List<LendingDto> getAllLendings() {
-    return LendingMapper.toDtoList(lendingService.getAll());
+  public List<LendingDto> getAlllending() {
+    return LendingMapper.toDtoList(lendingervice.getAll());
   }
 
   @GetMapping("/{id}")
   public LendingDto getLendingById(@PathVariable Long id) {
-    return LendingMapper.toDto(lendingService.getById(id));
+    return LendingMapper.toDto(lendingervice.getById(id));
   }
 
   @PostMapping
   public LendingDto createLending(@RequestBody LendingDto lendingDto) {
-    return LendingMapper.toDto(lendingService.create(LendingMapper.toEntity(lendingDto)));
+    return LendingMapper.toDto(lendingervice.create(LendingMapper.toEntity(lendingDto)));
   }
 
   @DeleteMapping("/{id}")
   public void deleteLending(@PathVariable Long id) {
-    lendingService.delete(id);
+    lendingervice.delete(id);
   }
 }
