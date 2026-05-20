b18840010f50a2372a62c55a2ec95ccc273b1eff
Slerdern <gweltaz.robert3112@gmail.com>
2026-05-20 09:49:13 +0200

    Fixtures fonctionnelles

---
+++ backend/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO app_user (id, username, email, password, rating) VALUES
+  (1, 'alice', 'alice@example.com', 'secret', 5);
+
+INSERT INTO object (id, name, owned_by_id, status, state) VALUES
+  (1, 'Chaise', 1, 'AVAILABLE', 'GOOD');
+
