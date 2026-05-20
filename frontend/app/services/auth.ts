65b4aca4b57d2d53094729a467cf3234b5654413
Lolippie <charlot.noemie72@gmail.com>
2026-05-20 21:00:39 +0200

    fix: api

--- frontend/app/services/auth.ts
+++ frontend/app/services/auth.ts
@@ -4,17 +4,17 @@
 import type { ICreateUserDto } from "~/dto/user/create.dto";
 import type { IUser, IUserAuth } from "~/types/user";
 
-const api = useApi()
+const api = () => useNuxtApp().$api
 
 export const login = async (email: string, password: string) => {
-    return await api<IUserAuth>("/auth/login", {
+    return await api()<IUserAuth>("/auth/login", {
         method: "POST",
         body: { email, password }
     });
 }
 
 export const register = async (createUserDto: ICreateUserDto) => {
-    return await api<IUser>("/auth/register", {
+    return await api()<IUser>("/auth/register", {
         method: "POST",
         body: createUserDto
     });
