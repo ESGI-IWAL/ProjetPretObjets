2efa2c6d639dda4bc7085aec4904239b8e320af4
Slerdern <gweltaz.robert3112@gmail.com>
2026-05-21 16:32:04 +0200

    Correction auth v3

--- backend/src/main/java/com/aliw/pretemoica/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/aliw/pretemoica/security/JwtAuthenticationFilter.java
@@ -1,11 +1,11 @@
 package com.aliw.pretemoica.security;
 
+import jakarta.annotation.Nullable;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.lang.Nullable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
